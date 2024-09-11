720641fef19d344fa9edc54f974031473c87daf4
Haard Shah <haard2014@outlook.com>
2024-09-11 21:51:00 +0200

    Created an Alert component

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -1,28 +1,33 @@
 import { createContext, useReducer } from "react";
-import alertReducer from './AlertReducer'
+import alertReducer from "./AlertReducer";
 
-const AlertContext = createContext()
+const AlertContext = createContext();
 
-export const AlertProvider = ({children}) => {
-    const initialState = null
+export const AlertProvider = ({ children }) => {
+  const initialState = null;
 
-    const [state, dispatch] = useReducer(alertReducer, initialState)
+  const [state, dispatch] = useReducer(alertReducer, initialState);
 
-    // Set an Alert
-    const setAlert = (msg, type) => {
-        dispatch({
-            type: 'SET_ALERT',
-            payload: {msg, type}
-        })
+  // Set an Alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: "SET_ALERT",
+      payload: { msg, type },
+    });
 
-        setTimeout(() => dispatch({type: 'REMOVE_ALERT'}), 3000)
-    }
+    setTimeout(() => dispatch({ type: "REMOVE_ALERT" }), 3000);
+  };
 
-    return <AlertContext.Provider 
-        value = {{
-            alert: state, 
-            setAlert
-        }}>
-            {children}
-        </AlertContext.Provider>
-}
+  return (
+    <AlertContext.Provider
+      value={{
+        alert: state,
+        setAlert,
+      }}
+    >
+      {children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertContext;
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,61 +1,61 @@
 import { createContext, useReducer } from "react";
 import githubReducer from "./GithubReducer";
 
-const GithubContext = createContext()
+const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-export const GithubProvider = ({children}) => {
-    const initialState = {
-        users: [],
-        loading: false,
-    }
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: false,
+  };
 
-    const [state, dispatch] = useReducer(githubReducer, initialState)
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
-    // Get Search Results
-    const searchUsers = async(text) => {
-        setLoading()
+  // Get Search Results
+  const searchUsers = async (text) => {
+    setLoading();
 
-        const params = new URLSearchParams({
-            q: text
-        })
+    const params = new URLSearchParams({
+      q: text,
+    });
 
-        const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization:`token ${GITHUB_TOKEN}`,
-            }
-        })
+    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
 
-        // destructure items from reponse
-        const {items} = await res.json()
+    // destructure items from reponse
+    const { items } = await res.json();
 
-        // dispatch the users to update state in context
-        dispatch({
-            type: 'GET_USERS',
-            payload: items,
-        })
-    }
+    // dispatch the users to update state in context
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
 
-    // Set Loading
-    const setLoading = () => dispatch({type: 'SET_LOADING'})
+  // Set Loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
-    // Clear Users from state
-    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+  // Clear Users from state
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
-    return (
-    <GithubContext.Provider 
-        value={{
-            users: state.users,
-            loading: state.loading,
-            searchUsers,
-            clearUsers
-            }}>
-                {children}
-        </GithubContext.Provider>
-    )
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
 
-}
-
-export default GithubContext
+export default GithubContext;
