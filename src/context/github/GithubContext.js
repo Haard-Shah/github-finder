d722da68f32e7b392755c83bf191472e25faa48d
Haard Shah <haard2014@outlook.com>
2024-09-12 20:35:45 +0200

    Refactored GithubContext functions into GithubActions.js file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,9 +3,6 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
@@ -16,99 +13,11 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get Search Results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    // destructure items from reponse
-    const { items } = await res.json();
-
-    if (items.length > 0) {
-      // dispatch the users to update state in context
-      dispatch({
-        type: "GET_USERS",
-        payload: items,
-      });
-    }
-  };
-
-  // Get single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const res = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (res.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await res.json();
-
-      // dispatch the users to update state in context
-      dispatch({
-        type: "GET_USER",
-        payload: data,
-      });
-    }
-  };
-
-  // Get User Repos
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10,
-    });
-
-    const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    // destructure items from reponse
-    const data = await res.json();
-
-    if (data.length > 0) {
-      // dispatch the users to update state in context
-      dispatch({
-        type: "GET_REPOS",
-        payload: data,
-      });
-    }
-  };
-
-  // Set Loading
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
-
-  // Clear Users from state
-  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos,
+        ...state,
+        dispatch,
       }}
     >
       {children}
